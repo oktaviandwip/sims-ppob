bfbe802580b0894f1b66fbd8e4ff24c595e98b1e
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-09-06 18:53:26 +0700

    Revise profile

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -9,10 +9,19 @@ upload.multer = multer({
   limits: { fieldSize: 100 * 1024 * 1024 },
 }).single("profile_image");
 
+upload(req, res, (err) => {
+  if (err instanceof multer.MulterError) {
+    return response(res, 500, err.message); // A Multer error occurred when uploading.
+  } else if (err) {
+    return response(res, 400, err.message); // An unknown error occurred when uploading.
+  }
+  next(); // Everything went fine.
+});
+
 // Cloudinary
 upload.cloudinary = async (req, res, next) => {
   if (!req.file) {
-    return res.status(400).send("No file uploaded");
+    return res.status(102).send("Tidak ada file yang diupload");
   }
 
   try {
