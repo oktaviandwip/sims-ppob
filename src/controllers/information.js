0ce2efab492d3ee6ccd674daeae4ee86109b7a59
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-09-06 19:28:01 +0700

    Revise information

--- src/controllers/information.js
+++ src/controllers/information.js
@@ -2,23 +2,23 @@ const controllers = {};
 const models = require("../models/information");
 const response = require("../utils/response");
 
-// Get Banner
+// Get banner
 controllers.getBanner = async (req, res) => {
   try {
     const result = await models.getBanner();
     return response(res, 0, "Sukses", result);
   } catch (err) {
-    return response(res, 500, err.message);
+    return response(res, 103, err.message);
   }
 };
 
-// Get Services
+// Get services
 controllers.getServices = async (req, res) => {
   try {
     const result = await models.getServices();
     return response(res, 0, "Sukses", result);
   } catch (err) {
-    return response(res, 500, err.message);
+    return response(res, 103, err.message);
   }
 };
 
